cfc86878702ad23c8692277275ca39166183f4d7
JB <jadon.binda@gmail.com>
2023-11-11 17:52:05 -0300

    build: add and config cors npm dependency

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = require('./scrapeAPI');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 const port = 3001;
